21b2a5ab43064136b2367be1d23a76a55d2d27f8
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-16 07:22:16 +0300

    init

--- CustomTopBarPicker/ContentView.swift
+++ CustomTopBarPicker/ContentView.swift
@@ -9,13 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var currentTab = 0
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Picker(selection: $currentTab, label: Text("")) {
+                Image(systemName: "house.fill").tag(0)
+                Image(systemName: "flame.fill").tag(1)
+                Image(systemName: "paperplane.fill").tag(2)
+                }.pickerStyle(SegmentedPickerStyle()).padding()
+            
+            Spacer()
+            
+            if currentTab == 0 {
+                page1()
+            }
+            if currentTab == 1 {
+                page2()
+            }
+            if currentTab == 2 {
+                page3()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct page1: View {
+    var body: some View {
+        Text("Page 1")
+    }
+}
+
+struct page2: View {
+    var body: some View {
+        Text("Page 2")
+    }
+}
+struct page3: View {
+    var body: some View {
+        Text("Page 3")
     }
 }
